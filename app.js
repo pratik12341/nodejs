7043758b84d9b3f01cb9a177375f7f8fa3ddce82
pratik12341 <pvwagiaonkar9@gmail.com>
2017-11-26 19:42:50 +0530

    working with files

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hello world");
+var fs = require('fs');
+
+/*
+//This is sync version of reading and writing file
+var readtext = fs.readFileSync('readme.txt','utf8');
+console.log(readtext);
+fs.writeFileSync('writeme.txt',readtext);
+*/
+
+var readtext = fs.readFile('readme.txt','utf8',function(err,data){
+  fs.writeFile('writeme.txt',data);
+});
