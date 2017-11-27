194c8e986ecb4021339958456dd909d25b29b1ef
pratik12341 <pvwagiaonkar9@gmail.com>
2017-11-27 12:55:56 +0530

    serving html content

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello world");
+var http = require('http');
+var fs = require('fs');
+var server = http.createServer(function(req,res){
+  res.writeHead('200',{'Content-Type':'text/html'});
+  var myReadStream = fs.createReadStream(__dirname + '/index.html');
+  myReadStream.pipe(res);
+})
+
+server.listen(3000,'127.0.0.1');
+console.log("server started on port 3000");
