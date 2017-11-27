f097ea4ad88a6747b57a6a3ee4230503b5caa7b8
pratik12341 <pvwagiaonkar9@gmail.com>
2017-11-27 12:07:43 +0530

    working with PIPES

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hello world");
+var fs = require('fs');
+var http = require('http');
+
+var server = http.createServer(function(req,res){
+  console.log('Request was made to : ' + req.url);
+  res.writeHead('200',{'Content-Type':'text/plain'});
+  var myReadStream = fs.createReadStream(__dirname +'/readme.txt');
+  myReadStream.pipe(res);
+});
+
+server.listen(3000,'127.0.0.1');
+console.log("server started listening on port 3000");
