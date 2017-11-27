7f15a737e3a77587aef54b354b170beb1d0e7964
pratik12341 <pvwagiaonkar9@gmail.com>
2017-11-27 11:00:53 +0530

    creating server

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello world");
+var http = require('http');
+
+var server = http.createServer(function(req,res){
+  console.log("request was made to : " + req.url);
+  res.writeHead('200',{'Content-Type':'text/plain'});
+  res.end("This is sample server");
+});
+
+server.listen(3000,'127.0.0.1');
+console.log("server started on port 3000");
