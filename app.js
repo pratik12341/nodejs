10490960a5dda1c5488d0849f06f53e6b712ec60
pratik12341 <pvwagiaonkar9@gmail.com>
2017-11-27 23:26:10 +0530

    express initial

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Hello world");
+var express = require('express');
+
+var app = express();
+
+app.get('/',function(req,res){
+  res.send("this is homepage");
+});
+
+app.get('/contact-us',function(req,res){
+  res.send("this is contact us page");
+});
+
+app.get('/profile/:id',function(req,res){
+  res.send("You have requested for the profile of " + req.params.id);
+});
+app.listen(3000);
