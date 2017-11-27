34ace61b785b9d23812339630bdc0b00670d4272
pratik12341 <pvwagiaonkar9@gmail.com>
2017-11-27 11:28:47 +0530

    readable streams

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Hello world");
+var http = require('http');
+var fs = require('fs');
+
+var myReadStream = fs.createReadStream(__dirname + '/readme.txt','utf8');
+
+myReadStream.on('data',function(chunk){
+  console.log("Reading new chunk of data");
+  console.log(chunk);
+})
