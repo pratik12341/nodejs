cfb960e785c14bd262ec29a870e427089b7db29b
pratik12341 <pvwagiaonkar9@gmail.com>
2017-11-27 13:15:17 +0530

    working with json

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Hello world");
+var http = require('http');
+
+var server = http.createServer(function(req,res){
+  res.writeHead('200',{'Content-Type':'text/json'});
+  var obj = {
+    name:'test',
+    country:'test1',
+    phone:'123456788'
+  }
+  res.end(JSON.stringify(obj));
+});
+
+server.listen(3000,'127.0.0.1');
+console.log("server started on port 3000");
